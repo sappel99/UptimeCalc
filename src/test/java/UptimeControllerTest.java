320340345e47196765e6c5b298f8633f50b2419c
sappel99 <simon.appel99@gmail.com>
2022-11-19 15:17:05 +0100

    first tests (unit test and live testing in browser works)

---
+++ src/test/java/UptimeControllerTest.java
@@ -0,0 +1,20 @@
+import at.sappel.UptimeController;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class UptimeControllerTest {
+
+    @Test
+    public void test_calculateUptimeCorrectly(){
+
+        //Arrange
+        UptimeController controller = new UptimeController();
+
+        //Act
+        String actualValue = controller.calculateUptime(99.95);
+
+        //Assert
+        Assertions.assertEquals("43178,40", actualValue);
+
+    }
+}
